6655ef1f6305765768830108caed30c13b4cde20
Asimina Athanasopoulou <asimina.ath@outlook.com>
2026-08-14 13:41:34 +0300

    Phase 1A : Initial commit MVVM architecture defined

---
+++ HideAndCheekyCar/Models/CarLocationModel.swift
@@ -0,0 +1,26 @@
+//
+//  CarLocation.swift
+//  HideAndCheekyCar
+//
+//  Created by Asimina Athanasopoulou on 11/07/2026.
+//
+
+
+import Foundation
+import CoreLocation
+
+struct CarLocation: Codable {
+    let id: UUID
+    let latitude: Double
+    let longitude: Double
+    var notes: String
+    let savedDate: Date
+    
+    init(latitude: Double, longitude: Double, notes: String = "") {
+        self.id = UUID()
+        self.latitude = latitude
+        self.longitude = longitude
+        self.notes = notes
+        self.savedDate = Date()
+    }
+}
